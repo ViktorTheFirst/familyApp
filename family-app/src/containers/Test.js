cb0cae091b8299f2de27b11f726820b6871116f5
Viktor Stepanenco <vityamv@gmail.com>
2021-02-02 23:21:43 +0200

    testing table for ses

--- family-app/src/containers/Test.js
+++ family-app/src/containers/Test.js
@@ -1,51 +1,480 @@
 import React from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import {
+  Chip,
+  Tooltip,
+  IconButton,
+  Checkbox,
+  Paper,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+  Toolbar,
+} from '@material-ui/core';
 
-class Test extends React.Component {
-  handleEditorChange = (content, editor) => {
-    console.log('Content was updated:', content);
-  };
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
-  render() {
-    return (
-      <Editor
-        initialValue=''
-        init={{
-          height: 500,
-          menubar: true,
-          plugins: [
-            'image advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount',
-          ],
-          file_picker_callback: function (cb, value, meta) {
-            let input = document.createElement('input');
-            input.setAttribute('type', 'file');
-            input.setAttribute('accept', 'image/*');
-
-            input.onchange = function () {
-              let file = this.files[0];
-              let reader = new FileReader();
-              let myURL = URL.createObjectURL(this.files[0]);
-              reader.onload = function () {
-                cb(myURL, { title: file.name, height: 'auto' });
-              };
-              reader.readAsDataURL(file);
-            };
-            input.click();
-          },
-          file_picker_types: 'image',
-          image_title: true,
-          automatic_uploads: true,
-          toolbar:
-            'undo redo | formatselect | bold italic backcolor | \
-             alignleft aligncenter alignright alignjustify | \
-             bullist numlist outdent indent | removeformat | help',
-        }}
-        onEditorChange={this.handleEditorChange}
-      />
-    );
-  }
+function createData(fullName, id, email, password, courses) {
+  return { fullName, id, email, password, courses };
 }
 
-export default Test;
+const rows = [
+  createData('Student_1', 1000, 'student_1@mail.com', 123456, [
+    {
+      id: '1',
+      title: 'Course_1',
+      desc: 'course_1 description and information',
+    },
+    {
+      id: '2',
+      title: 'Course_2',
+      desc: 'course_2 description and information',
+    },
+  ]),
+  createData('Student_2', 1001, 'student_2@mail.com', 123456, [
+    {
+      id: '1',
+      title: 'Course_1',
+      desc: 'course_1 description and information',
+    },
+    {
+      id: '2',
+      title: 'Course_2',
+      desc: 'course_2 description and information',
+    },
+  ]),
+  createData('Student_3', 1002, 'student_3@mail.com', 123456, [
+    {
+      id: '1',
+      title: 'Course_1',
+      desc: 'course_1 description and information',
+    },
+    {
+      id: '2',
+      title: 'Course_2',
+      desc: 'course_2 description and information',
+    },
+    {
+      id: '3',
+      title: 'Course_3',
+      desc: 'course_3 description and information',
+    },
+    {
+      id: '4',
+      title: 'Course_4',
+      desc: 'course_4 description and information',
+    },
+  ]),
+  createData('Student_4', 1003, 'student_4@mail.com', 123456, [
+    {
+      id: '1',
+      title: 'Course_1',
+      desc: 'course_1 description and information',
+    },
+    {
+      id: '2',
+      title: 'Course_2',
+      desc: 'course_2 description and information',
+    },
+    {
+      id: '3',
+      title: 'Course_3',
+      desc: 'course_3 description and information',
+    },
+  ]),
+  createData('Student_5', 1004, 'student_5@mail.com', 123456, [
+    {
+      id: '1',
+      title: 'Course_1',
+      desc: 'course_1 description and information',
+    },
+  ]),
+];
+
+const colors = ['#ff7043', '#ba68c8', '#42a5f5', '#009688', '#ff4081'];
+const handleDeleteCourse = () => {
+  console.info('You clicked the delete icon.');
+};
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: 'fullName',
+    numeric: false,
+    disablePadding: true,
+    label: 'Full Name',
+  },
+  { id: 'id', numeric: true, disablePadding: false, label: 'ID#' },
+  { id: 'email', numeric: true, disablePadding: false, label: 'Email Address' },
+  { id: 'password', numeric: true, disablePadding: false, label: 'Password' },
+  {
+    id: 'courses',
+    numeric: true,
+    disablePadding: false,
+    label: 'Assigned Courses',
+  },
+];
+
+function EnhancedTableHead(props) {
+  const {
+    classes,
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+  } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding='checkbox'>
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{ 'aria-label': 'select all desserts' }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+              style={{ fontWeight: 'bold' }}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  title: {
+    flex: '1 1 100%',
+  },
+}));
+
+const EnhancedTableToolbar = (props) => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          className={classes.title}
+          color='inherit'
+          variant='subtitle1'
+          component='div'
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          className={classes.title}
+          variant='h6'
+          id='tableTitle'
+          component='div'
+        >
+          Nutrition
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <>
+          <Tooltip title='Delete Student'>
+            <IconButton aria-label='delete'>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title='Add Course'>
+            <IconButton aria-label='add_course'>
+              <AddCircleOutlineIcon />
+            </IconButton>
+          </Tooltip>
+        </>
+      ) : (
+        <Tooltip title='Filter list'>
+          <IconButton aria-label='filter list'>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  paper: {
+    width: '80%',
+    marginBottom: theme.spacing(2),
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+//------------------------------------------------------------------------------------
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.fullName);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby='tableTitle'
+            size={'medium'}
+            aria-label='enhanced table'
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.fullName);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.fullName)}
+                      role='checkbox'
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.fullName}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding='checkbox'>
+                        <Checkbox
+                          checked={isItemSelected}
+                          inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                      </TableCell>
+                      <TableCell
+                        component='th'
+                        id={labelId}
+                        scope='row'
+                        padding='none'
+                      >
+                        {row.fullName}
+                      </TableCell>
+
+                      <TableCell align='right'>{row.id}</TableCell>
+                      <TableCell align='right'>{row.email}</TableCell>
+                      <TableCell align='right'>{row.password}</TableCell>
+
+                      <TableCell align='right'>
+                        {row.courses.map((course) => (
+                          <Tooltip
+                            key={course.id}
+                            title={
+                              <>
+                                <Typography>
+                                  <b>{course.title}</b>
+                                </Typography>
+                                <Typography>{course.desc}</Typography>
+                              </>
+                            }
+                          >
+                            <Chip
+                              label={course.id}
+                              onDelete={handleDeleteCourse}
+                              style={{
+                                backgroundColor: colors[course.id],
+                              }}
+                            />
+                          </Tooltip>
+                        ))}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component='div'
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
+  );
+}
